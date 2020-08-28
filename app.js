928951a4d15a973f4cae331771c45cd2659bd879
ValentinaTironi <valentinatironi5@gmail.com>
2020-08-28 20:38:39 -0300

    uwu

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var app = express();
 
 const recetas = [];
+const findRecipe = (receta_id) => recetas.find((receta) => receta.id === receta_id)
 
 app.use(function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "http://localhost:3000"); // update to match the domain you will make the request from
@@ -10,25 +11,21 @@ app.use(function (req, res, next) {
 });
 
 app.get('/recipe/:recipekey/like/:review', function (req, res) {
-  var foundRecipe = recetas.find(function (receta) {
-    return receta.id == req.params.recipekey
-  });
-
-  foundRecipe.likes = (foundRecipe.likes + parseInt(req.params.review)) / 2
+  let recipe = findRecipe(req.params.recipekey);
+  recipe.likes = (recipe.likes + parseInt(req.params.review)) / 2
 });
 
 
 app.get('/recipe/:recipekey', function (req, res) {
-  var foundRecipe = recetas.find(function (receta) {
-    return receta.id == req.params.recipekey
-  })
-  if (foundRecipe == undefined) {
-    const newRecipe = { id: req.params.recipekey, likes: 5 }
+  let recipe = findRecipe(req.params.recipekey);
+
+  if (!recipe) {
+    let newRecipe = { id: req.params.recipekey, likes: 5 }
     recetas.push(newRecipe);
     return res.json(newRecipe);
-
   }
-  return res.json(foundRecipe);
+
+  return res.json(recipe);
 });
 
 app.get('/read', function (req, res) {
